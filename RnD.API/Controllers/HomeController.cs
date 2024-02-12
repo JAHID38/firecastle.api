2a27a1045f977828f27c77b7efa8c643c698ac93
Jahid <zahidmuhammad095@gmail.com>
2024-02-12 16:26:43 +0600

    RnD project initiated with clean-up

---
+++ RnD.API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RnD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> CurrentDateTime()
+        {
+            var response = await Task.FromResult(DateTime.Now.ToLongDateString());
+            return Ok(response);
+        }
+    }
+}
