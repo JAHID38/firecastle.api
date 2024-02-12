cc978ae6f0cc2a8cc9483e168bb5e7823860ca53
jahid.shadhin <jahid@shadhinmusic.com>
2024-02-12 16:55:46 +0600

    Read from appsettings.json using Option pattern

--- RnD.API/Controllers/HomeController.cs
+++ RnD.API/Controllers/HomeController.cs
@@ -1,18 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using RnD.API.Options;
 
 namespace RnD.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HomeController : ControllerBase
+    public class HomeController
+        (
+            IOptions<LoggerOption> logger
+        )
+        : ControllerBase
     {
+        private readonly LoggerOption _logger = logger.Value;
+
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> CurrentDateTime()
         {
             var response = await Task.FromResult(DateTime.Now.ToLongDateString());
             return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("status")]
+        public async Task<IActionResult> GetStatusAsync([FromQuery] string? param)
+        {
+            var response = param switch
+            {
+                "logger" => _logger.EnableLog ? "Enabled" : "Disabled",
+                _ => throw new NotImplementedException()
+            };
+            return Ok(await Task.FromResult(response));
         }
     }
 }
